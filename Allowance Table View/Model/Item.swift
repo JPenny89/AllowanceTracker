42f82a144df1e204c76984dc0f0350101f0c7d76
James Penny <james_penny@hotmail.co.uk>
2022-12-02 16:01:26 +0000

    Created basic TableView version and able to add/remove items. This data also persists once the app is terminated.

---
+++ Allowance Table View/Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Allowance Table View
+//
+//  Created by James Penny on 02/12/2022.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
