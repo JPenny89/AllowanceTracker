2c46f93e6d9a874be74618f10e6370cd7688ffbe
James Penny <james_penny@hotmail.co.uk>
2022-12-02 15:21:28 +0000

    Initial Commit

---
+++ Allowance Table View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Allowance Table View
+//
+//  Created by James Penny on 02/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
