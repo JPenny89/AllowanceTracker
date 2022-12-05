acdf16e07cee46a8e0487b1f153b458b977fcb7a
James Penny <james_penny@hotmail.co.uk>
2022-12-05 13:08:14 +0000

    Change from a Table VC to a standard VC with a TableView within that. Deleted original Table VC Moved TableView delegate & datasource methods to extensions. Added functionality to delete table entries.

--- Allowance Table View/Controllers/TransactionsViewController.swift
+++ Allowance Table View/Controllers/TransactionsViewController.swift
@@ -7,7 +7,10 @@
 
 import UIKit
 
-class TransactionsViewController: UITableViewController {
+class TransactionsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
     
     var itemArray = [Item]()
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
@@ -21,40 +24,9 @@ class TransactionsViewController: UITableViewController {
         
     }
     
-    //MARK: - Tablewiew Datasource Methods
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
-    }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        
-        let item = itemArray[indexPath.row]
-        
-        cell.textLabel?.text = item.title
-        
-        cell.accessoryType = item.done ? .checkmark : .none
-        
-        return cell
-        
-    }
-    
-    //MARK: - TableView Delegate Methods
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        
-        saveItems()
-        
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
-    
-    
     //MARK: - Add New Items
+    
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -80,7 +52,9 @@ class TransactionsViewController: UITableViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
     }
+    
     
     //MARK: - Model Manipulation Methods
     
@@ -109,4 +83,50 @@ class TransactionsViewController: UITableViewController {
     }
     
 }
+
+
+
+//MARK: - Tablewiew Datasource Methods
+
+extension TransactionsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        return cell
+        
+    }
+
+    //MARK: - TableView Delegate Methods
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        itemArray.remove(at: indexPath.row)
+      tableView.deleteRows(at: [indexPath], with: .automatic)
+    }
+    
+    
+}
+
+
+
 
